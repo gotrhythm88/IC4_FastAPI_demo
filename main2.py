467620fc29c68dbc99c51d09234571ebe7e57c0e
Tamasin Ford <tamasin.ford@gmail.com>
2021-12-09 19:55:38 -0600

    heroku set up

--- main2.py
+++ main2.py
@@ -1,4 +1,5 @@
 from typing import Optional
+
 from fastapi import FastAPI
 import random
 
@@ -7,45 +8,41 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Greeting": "This is an RPS player"}
+    return {"Hello": "World"}
 
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
 
 @app.get("/rps_play/")
 def play_rps():
     api_choice = random.choice(["rock","paper","scissors"])
-    return {"api_play": api_choice, "status": "success"}
+    return {"play": api_choice}
 
 @app.get("/rps_score/player/{player_choice}")
 def score_rps(player_choice: str):
     api_choice = random.choice(["rock","paper","scissors"])
-    score = {}
-    score["result"] = scoregame(api_choice, player_choice)
-    score["api"] = api_choice
-    score["status"] = "success"
-    return score
+    scoregame(api_choice, player_choice)
 
 @app.get("/rps_score/player/{player_choice}/api/{api_choice}")
 def score_rps(player_choice: str, api_choice: str):
-    score = {}
-    score["result"] = scoregame(api_choice, player_choice)
-    score["api"] = api_choice
-    score["status"] = "success"
-    return score
+    scoregame(api_choice, player_choice)
 
 def scoregame(api_choice, player_choice):
     if api_choice==player_choice:
-        return "tie"
+        return {"result": "tie"}
     elif api_choice=="rock" and player_choice=="paper":
-        return "player"
+        return {"result": "player"}
     elif api_choice=="rock" and player_choice=="scissors":
-        return "api"
+        return {"result": "api"}
     elif api_choice=="paper" and player_choice=="scissors":
-        return "player"
+        return {"result": "player"}
     elif api_choice=="paper" and player_choice=="rock":
-        return "api"
+        return {"result": "api"}
     elif api_choice=="scissors" and player_choice=="rock":
-        return "player"
+        return {"result": "player"}
     elif api_choice=="scissors" and player_choice=="paper":
-        return "api"
+        return {"result": "api"}
     else:
-        return "unable to determine winner"
+        return {"result": "unable to determine winner"}
